021070ee40f366410198a93b75112d4f78066cdd
sykong <sykong@syscon.kr>
2023-05-19 17:59:59 +0900

    테스트 순서 지정

--- src/test/java/com/mc/testexample/basic/InstanceTest.java
+++ src/test/java/com/mc/testexample/basic/InstanceTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+// JUnit은 기본 전략으로 테스트 메소드마다 테스트 인스턴스를 새로 만든다.
+// @TestInstance(TestInstance.Lifecycle.PER_CLASS)를 사용하면 클래스마다 인스턴스를 만들도록 전략을 변경할 수 있다.
+// 이 때는 @BeforeAll, @AfterAll을 static으로 만들지 않아도 된다.
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class InstanceTest {
 
---
+++ src/test/java/com/mc/testexample/basic/OrderTest.java
@@ -0,0 +1,34 @@
+package com.mc.testexample.basic;
+
+import org.junit.jupiter.api.*;
+
+// Order는 낮을 수록 우선순위가 높다.
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class OrderTest {
+
+    int value = 1;
+
+    @Test
+    @Order(3)
+    void check_instance1(){
+        System.out.println("1 : " + this.toString() +", value : " + (value++));
+    }
+
+    @Test
+    @Order(1)
+    void check_instance2(){
+        System.out.println("2 : " + this.toString() +", value : " + (value++));
+    }
+
+    @Test
+    @Order(2)
+    void check_instance3() {
+        System.out.println("3 : " + this.toString() + ", value : " + (value++));
+    }
+
+    @Test
+    @Order(1)
+    void check_instance4() {
+        System.out.println("4 : " + this.toString() + ", value : " + (value++));
+    }
+}
