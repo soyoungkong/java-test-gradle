b910048bfa38bf191f2d18e7d8cce8018b57a5d4
sykong <sykong@syscon.kr>
2023-05-19 14:10:12 +0900

    커스텀 태그 테스트

---
+++ src/test/java/com/mc/testexample/basic/CustomTagTest.java
@@ -0,0 +1,37 @@
+package com.mc.testexample.basic;
+
+import com.mc.testexample.basic.custom.DevTest;
+import com.mc.testexample.basic.custom.LocalTest;
+import com.mc.testexample.basic.custom.ProdTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CustomTagTest {
+
+    @Test
+    @LocalTest
+    void create_new_robot_all(){
+        System.out.println("tag - local 테스트");
+        assertTrue(1==1, "테스트 용도");
+    }
+
+    @Test
+    @DevTest
+    @DisplayName("속도가 빠른 테스트")
+    void create_new_robot_fast(){
+        System.out.println("tag - dev 테스트");
+    }
+
+    @Test
+    @ProdTest
+    @DisplayName("속도 느린 테스트")
+    void create_new_robot_slow(){
+        System.out.println("tag - prod 테스트");
+    }
+}
