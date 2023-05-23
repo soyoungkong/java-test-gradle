26a2d219c905840f58f18d062acec70a778913d4
sykong <sykong@syscon.kr>
2023-05-23 17:26:42 +0900

    Testcontainer 실습

--- src/main/java/com/mc/testexample/robot/RobotRepository.java
+++ src/main/java/com/mc/testexample/robot/RobotRepository.java
@@ -1,6 +1,8 @@
 package com.mc.testexample.robot;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface RobotRepository extends JpaRepository<Robot, Long> {
 }
